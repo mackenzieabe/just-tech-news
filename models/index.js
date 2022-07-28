d9d99b9287022962f7130a2da3b019ad3d2899d2
mackgabe <mackenzieabe@gmail.com>
2022-07-28 17:46:38 -0400

    added comments route/model

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Post = require('./Post');
 const Vote= require('./Vote');
+const Comment= require('./Comment');
 
 // create associations
 //A user can make many posts. But a post only belongs to a single user, and never many users.
@@ -40,4 +41,20 @@ User.hasMany(Post, {
     foreignKey: 'post_id'
   });
 
-module.exports = { User, Post, Vote };
+  Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+  Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+  });
+  
+  User.hasMany(Comment, {
+    foreignKey: 'user_id'
+  });
+  
+  Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+  });
+
+module.exports = { User, Post, Vote, Comment};
